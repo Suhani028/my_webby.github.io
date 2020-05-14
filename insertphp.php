ec860687a95b3d6725868695f605210c4647843b
Suhani028 <58427939+Suhani028@users.noreply.github.com>
2020-05-14 22:15:18 +0530

    Add files via upload

---
+++ insertphp.php
@@ -0,0 +1,11 @@
+<<?php 
+$con = mysqli_connect('localhost','root');
+mysqli_select_db($con,'ajaxcurd');
+
+extract($_POST);
+
+if (isset($_POST['submit'])) 
+	$q = " insert into ajaxinsert (username,password) values ('$username','$password')";
+	$query = mysqli_query($con,$q);
+	header('location:index.html')
+?>
